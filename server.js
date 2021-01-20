c4571fc0e7ed1ab31e41a3b66df82febc1d35c25
Ehugo2000 <erik.hgsn@gmail.com>
2021-01-20 15:22:02 +0100

    displays a list of books.

---
+++ server.js
@@ -0,0 +1,12 @@
+const superstatic = require('superstatic')
+const browserSync = require('browser-sync')
+
+// hot reload
+browserSync.init({
+  server: {
+    middleware: [superstatic({ stack: 'strict' })],
+  },
+  port: 3474,
+  watch: true,
+  files: ['*.html', '*.js', '*.css'],
+})
